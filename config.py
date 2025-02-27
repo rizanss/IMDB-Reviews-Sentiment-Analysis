34b46727475f0fed20e3455b8c37ce58424489bd
Riza Nursyah <95666383+rizanss@users.noreply.github.com>
2025-02-27 12:41:24 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+MODEL_PATHS = {
+    "logistic_regression": "models/logreg_model_sentiment.pkl",
+    "lstm": "models/lstm_model_sentiment.h5",
+    "vectorizer": "models/vectorizer_logreg_sentiment.pkl",
+    "tokenizer": "models/tokenizer_lstm_sentiment.pkl"
+}
+
+API_SETTINGS = {
+    "host": "127.0.0.1",
+    "port": 8000
+}
